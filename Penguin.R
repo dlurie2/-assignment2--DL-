4257401a86bf72915843a0abc35e3198fc9466f8
dlurie2 <dlurie@wesleyan.edu>
2025-10-15 09:12:58 -0400

    Added regression analysis and plot

--- Penguin.R
+++ Penguin.R
@@ -17,5 +17,15 @@ penguins %>%
     count = n()
   )
 
+#Added Linear Regression
+model <- lm(body_mass_g ~ flipper_length_mm, data = penguins)
+summary(model)
 
-
+#Regression of Mass on Flipper Length
+library(ggplot2)
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "blue") +
+  labs(title = "Regression of Body Mass on Flipper Length",
+       x = "Flipper Length (mm)",
+       y = "Body Mass (g)")
